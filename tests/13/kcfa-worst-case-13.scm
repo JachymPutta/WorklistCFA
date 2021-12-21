8f8a09eca5c508cec91a434f652b769e7dbeb529
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2021-12-21 21:16:35 +0800

    Removed tests, added git ignore

--- tests/13/kcfa-worst-case-13.scm
+++
@@ -1,3 +0,0 @@
-; n = 13
-; # terms = 301
-((lambda (f1) (f1 #f)) (lambda (x1) ((lambda (f2) (f2 #t)) (lambda (x2) ((lambda (f3) (f3 #t)) (lambda (x3) ((lambda (f4) (f4 #f)) (lambda (x4) ((lambda (f5) (f5 #t)) (lambda (x5) ((lambda (f6) (f6 #f)) (lambda (x6) ((lambda (f7) (f7 #f)) (lambda (x7) ((lambda (f8) (f8 #f)) (lambda (x8) ((lambda (f9) (f9 #t)) (lambda (x9) ((lambda (f10) (f10 #f)) (lambda (x10) ((lambda (f11) (f11 #t)) (lambda (x11) ((lambda (f12) (f12 #t)) (lambda (x12) ((lambda (f13) (f13 #f)) (lambda (x13) ((lambda (z) (z x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13)) (lambda (y1 y2 y3 y4 y5 y6 y7 y8 y9 y10 y11 y12 y13) y1))))))))))))))))))))))))))))
--- tests/canonical/program.scm
+++
@@ -1,4 +0,0 @@
-(let((id (lambda(input dummy) (dummy input input))))
-  (let((zero (id (lambda(z) z) id)))
-    (let((one (id (lambda(succ z)(succ z)) id)))
-      zero)))
